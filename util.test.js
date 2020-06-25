9c7b59cc8abab8c18e2a6714a412ac3de34bd01e
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-25 14:22:03 -0400

    e2e test

--- util.test.js
+++ util.test.js
@@ -1,3 +1,5 @@
+const puppeteer = require("puppeteer");
+
 const { generateText, checkAndGenerate } = require("./util");
 
 test("Output title and price", () => {
@@ -7,5 +9,21 @@ test("Output title and price", () => {
 
 test("check and generate input", () => {
     const text = checkAndGenerate("Book1", 45);
-    expect(text).toBe('Book1 45')
+    expect(text).toBe("Book1 45");
+});
+
+test("clicked", async() => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 20,
+        args: ["--windows-size=1600,900"],
+    });
+
+    const page = await browser.newPage()
+    await page.goto('http://127.0.0.1:5500')
+    await page.click("#title")
+    await page.type('#title', 'Book')
+    await page.click('#price')
+    await page.type("#price", "79")
+    await page.click("#add-product")
 });
